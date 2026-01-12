d4749eac9e020eaf8fbbce256613bf388d6d477b
Eshita Kapat <eshita.kapat@outlook.com>
2026-01-12 16:18:25 +0530

    Async Handler SetUp

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,15 +1,21 @@
 import {ApiResponse} from "../utils/api-response.js"
+import { asyncHandler } from "../utils/async-handler.js"
 
-const healthcheck = (req , res) => {
+/** 
+const healthcheck = async (req , res , next) => {
 try {
-    
+    const user = await getUserFromDB()
     res
     .status(200)
     .json(new ApiResponse(200 , {message: "Server is running"}))
     
 } catch (error) {
-    
+    next(err)
 }
 }
+*/
 
+const healthcheck = asyncHandler(async(req , res) => {
+    res.status(200).json(new ApiResponse(200 , {message: "Server is still running"}))
+})
 export {healthcheck}
