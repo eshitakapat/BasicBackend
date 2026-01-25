172b5b2732c8c9a9a373a713b982b808ef12769a
Eshita Kapat <eshita.kapat@outlook.com>
2026-01-25 23:19:47 +0530

    Project Controllers

---
+++ src/controllers/project.controllers.js
@@ -0,0 +1,50 @@
+import { User } from "../models/user.models.js";
+import { Project } from "../models/project.models.js";
+import { ProjectMember } from "../models/task.models.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-errors.js";
+import { asyncHandler } from "../utils/async-handler.js";
+
+const getProjects = asyncHandler(async (req , res) => {
+
+})
+
+const getProjectsById = asyncHandler(async (req , res) => {
+    
+})
+
+const createProject = asyncHandler(async (req , res) => {
+    
+})
+
+const updateProject = asyncHandler(async (req , res) => {
+    
+})
+
+const deleteProject = asyncHandler(async (req , res) => {
+    
+})
+
+const addMembersToProject = asyncHandler(async (req , res) => {
+    
+})
+
+const getProjectMembers = asyncHandler(async (req , res) => {
+    
+})
+
+const updateMemberRole = asyncHandler(async (req , res) => {
+    
+})
+
+const deleteMember = asyncHandler(async (req , res) => {
+    
+})
+
+export {
+    addMembersToProject , createProject , deleteMember , getProjects , getProjectsById, getProjectMembers , updateMemberRole , deleteProject , updateProject
+}
+
+
+
+
