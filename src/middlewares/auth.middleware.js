82049e529e9d389f7850b706f14c70c0626ecc57
Eshita Kapat <eshita.kapat@outlook.com>
2026-01-28 20:22:24 +0530

    Role Based Permission Controller

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.models.js";
+import { ProjectMember } from "../models/user.models.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-errors.js";
 import jwt from "jsonwebtoken";
@@ -34,3 +35,36 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, "Invalid or expired Access Token");
   }
 });
+
+
+export const validateProjectPermission = (roles = []) => {
+asyncHandler(async(req, res,next) => {
+  const {projectId} = req.params;
+
+  if (!projectId) {
+    throw new ApiError(400 , "project id is missing")
+  }
+
+
+  const project = await ProjectMember.findOne({
+    project: new mongoose.Types.ObjectId(projectId),
+    user : new mongoose.Types.ObjectId(req.user._id)
+  })
+
+   if (!project) {
+    throw new ApiError(400 , "project not found")
+  }
+
+  const givenRole = project?.role
+
+  req.user.role = givenRole
+
+  if (roles.includes(givenRole)) {
+    throw new ApiError(
+      403, "You do not have permission to perform this action"
+    )
+  }
+
+  next()
+})
+}
